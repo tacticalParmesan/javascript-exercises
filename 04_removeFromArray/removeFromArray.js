e4fba7c8b11cdc21d66a09b1fd48deb8174f0970
tacticalParmesan <edoardoferreri10@gmail.com>
2024-01-20 15:31:10 +0100

    Solved 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayToAlter, ...itemsToRemove) {
+  for (const item of itemsToRemove) {
+    const itemToRemoveIdx = arrayToAlter.indexOf(item);
+    if (itemToRemoveIdx !== -1) {
+      arrayToAlter.splice(itemToRemoveIdx, 1)
+    } 
+  }
+  return arrayToAlter;
 };
 
 // Do not edit below this line
