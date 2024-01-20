fa77bde1b9353fa4e79deabb14ff482e10fa3d2a
tacticalParmesan <edoardoferreri10@gmail.com>
2024-01-20 14:48:12 +0100

    Solved 02ù

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+  let repeatedString = "";
+  if (times < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < times; i++) {
+      repeatedString += word;
+    }
+  }
+    
+  return repeatedString;
 };
 
 // Do not edit below this line
