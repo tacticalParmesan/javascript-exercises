fc4bcaa811e32d2bbcd3498b4629b4e7d04d07b4
tacticalParmesan <edoardoferreri10@gmail.com>
2024-01-20 16:16:20 +0100

    Solved 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFarenheit) {
+  const conversionFormula = (tempInFarenheit - 32) * 5/9;
+  let convertedTemp = (Math.round(conversionFormula * 10) / 10).toFixed(1);
+  return Number(convertedTemp);
+   
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  const conversionFormula = (tempInCelsius * 9/5) + 32
+  let convertedTemp = (Math.round(conversionFormula * 10) / 10).toFixed(1);
+  return Number(convertedTemp);
 };
 
 // Do not edit below this line
