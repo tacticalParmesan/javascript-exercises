5a854350d2d61fddfb19e302c1f3a5d2ff1e121a
tacticalParmesan <edoardoferreri10@gmail.com>
2024-01-20 17:45:40 +0100

    Solved 09 - Had to look up Regex

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+  const processedText = text.toLowerCase().replace(/[^a-z0-9]/g, "")
+  return processedText.split("").reverse().join("") === processedText;
 };
 
 // Do not edit below this line
