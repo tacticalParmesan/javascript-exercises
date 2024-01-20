2510dbb9ca91090e341aff873d4d3f72105ef86e
tacticalParmesan <edoardoferreri10@gmail.com>
2024-01-20 15:08:39 +0100

    Solved 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(text) {
+  let reversedString = "";
 
+  for (let i = text.length - 1; i >= 0; i--) {
+    reversedString += text[i];
+  }
+  return reversedString;
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
