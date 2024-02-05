85d515180eae260c2294530607f64bee35e04f23
tacticalParmesan <edoardoferreri10@gmail.com>
2024-02-05 18:43:11 +0100

    Finished all Fundamentals exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
 
-};
+    people.forEach( (person) => {
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = currentYear;
+        }
+    })
+
+    const sortedbyOldest = people.sort( (a, b) => {
+
+        const aLifetime = a.yearOfDeath - a.yearOfBirth;
+        const bLifetime = b.yearOfDeath - b.yearOfBirth;
+        if (aLifetime < bLifetime) {
+            return 1;
+        } else {
+            return -1;
+        }
+    } )
+    return sortedbyOldest[0]
+}
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
